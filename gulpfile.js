56ac7b7af9d449873bc7192a777369e8fe5af3a1
Glenn Nicoll <glennnicoll@gmail.com>
2019-11-20 16:59:12 +1100

    Bootstrap setup

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,9 @@ var sass = require('gulp-sass');
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+return gulp.src(['src/scss/graemenicollart.scss'])
 .pipe(sass())
-.pipe(gulp.dest("src/css"))
+.pipe(gulp.dest("src/css/"))
 .pipe(browserSync.stream());
 });
 
